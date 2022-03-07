17c13ac39fcc3958e828a1f365b34dc1104e8791
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-07 19:35:08 +0300

    add accouts urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('registration.backends.simple.urls'))
+    path('', include('account.urls')),
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(), {"next_page": ''}),
+    path('tinymce/', include('tinymce.urls')),
 ]
