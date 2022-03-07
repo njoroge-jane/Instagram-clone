c4b2f1d49c6156bbce4a388dbe86ea885adc9c1e
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-08 01:11:54 +0300

    create basic html structures

--- instagram/settings.py
+++ instagram/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account.apps.AccountConfig'
+    'account.apps.AccountConfig',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
