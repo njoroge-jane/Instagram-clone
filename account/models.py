06b893a7b7061a89464ad89d51e9d13ce6d131d5
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-09 04:25:12 +0300

    edit image model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import CharField
+from tinymce import HTMLField
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    caption = models.TextField() 
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/')
