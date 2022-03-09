016321dd5847f3339756e0da206293fae11cb860
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-09 05:21:57 +0300

    create view point of upload form

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,9 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-path('', views.index, name='home')
+path('', views.index, name='home'),
+path('new/upload', views.upload, name='upload')
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- account/views.py
+++ account/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .forms import UploadImageForm
 
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def index(request):
   return render(request,'index.html')
+
+
+def upload(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.user = current_user
+            upload.save()
+        return redirect('index')
+
+    else:
+        form = UploadImageForm()
+    return render(request, 'new_upload.html', {"form": form})
+  
