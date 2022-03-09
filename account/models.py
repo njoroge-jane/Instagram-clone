289896b56a68d9e142615937c2bc64d1a774a60b
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-09 11:52:12 +0300

    create database

--- account/models.py
+++ account/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Image(models.Model):
-    title = models.CharField(max_length =60)
-    caption = models.TextField() 
-    pub_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to = 'images/') 
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,4 +10,11 @@ class Profile(models.Model):
     bio = models.TextField()
 
     def __str__(self):
-        return self.user.username    
+        return self.user.username  
+
+class Image(models.Model):
+    title = models.CharField(max_length =60)
+    caption = models.TextField() 
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/')
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)            
