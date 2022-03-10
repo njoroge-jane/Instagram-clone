1aece4c91394b91cf6a97a2bb6d3d65d0874636c
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-03-10 17:26:38 +0300

    deployment

--- account/models.py
+++ account/models.py
@@ -50,8 +50,8 @@ class Image(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to = 'images/')
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE) 
-    # comments = models.ForeignKey(Comments,on_delete=models.CASCADE)
-    # likes = models.ForeignKey(Likes,on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comments,on_delete=models.CASCADE,null=True)
+    likes = models.ForeignKey(Likes,on_delete=models.CASCADE,null=True)
 
     def save_image(self):
         self.save()  
